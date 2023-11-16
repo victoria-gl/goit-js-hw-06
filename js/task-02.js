138b71789dc7d868113ff8677fbd25458adfb3ec
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-16 23:19:46 +0100

    done-task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("ul#ingredients")
+console.log(list)
+
+
+
+ingredients.forEach((ingredient) => {
+  const element = document.createElement("li");
+  element.textContent = `${ingredient}`;
+  element.classList.add("item");
+  list.append(element)
+  
+});
