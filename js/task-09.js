0330f6a219ac172c6406418282f44564032518f6
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-17 14:51:16 +0100

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,22 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const div = document.querySelector(".widget")
+console.log(div)
+
+const colorSpan = document.querySelector(".color")
+console.log(colorSpan)
+
+const button = document.querySelector(".change-color")
+console.log(button)
+
+
+button.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
+
