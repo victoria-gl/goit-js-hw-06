c9d9b7ef2435a2320098aedabf14161217825dee
victoria-gl <138527509+victoria-gl@users.noreply.github.com>
2023-11-17 13:36:30 +0100

    task-09

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert('Всі поля повинні бути заповнені');
+    return;
+  }
+
+  const data = {
+    email: email,
+    password: password
+  };
+
+  console.log(data);
+  form.reset();
+});
